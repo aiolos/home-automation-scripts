5dc51d26ea93b9559a9c72554c4fa512e1ab11a3
Henri de Jong <henridejong@gmail.com>
2017-10-19 09:01:39 +0100

    Added scripts to be used in kodi for controlling other devices by IR

---
+++ kodi/scripts/volumeDown.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -o /dev/null http://192.168.1.177/?code=A55AD02F\&p=2\&r=3\&f=40\&d=20
---
+++ kodi/scripts/volumeUp.py
@@ -0,0 +1,3 @@
+import urllib2
+urllib2.urlopen("http://192.168.1.177/?code=A55A50AF&p=2&r=3&f=40&d=20").read()
+
