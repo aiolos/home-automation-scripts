bfc3d04b2030b6a75b0a8106663fed3594f9e92a
Henri de Jong <henridejong@gmail.com>
2017-11-03 15:26:13 +0000

    Updated lua script for power calculations

--- lua/script_device_power.lua
+++ lua/script_device_power.lua
@@ -2,16 +2,17 @@
 --
 commandArray = {}
 
-if devicechanged['Omnik 715'] or devicechanged['Omnik 725'] or devicechanged['Diehl (Youless)'] then
-   omnik715 = otherdevices_svalues['Omnik 715']
-   omnik725 = otherdevices_svalues['Omnik 725']
-   diehl = otherdevices_svalues['Diehl (Youless)']
+if devicechanged['Omnik 715'] or devicechanged['Omnik 725'] or devicechanged['Diehl (Youless)'] or devicechanged['Goodwe'] then
    omnik715Power, omnik715Energy = string.match(otherdevices_svalues['Omnik 715'], "(%d+%.*%d*);(%d+%.*%d*)")
    omnik725Power, omnik725Energy = string.match(otherdevices_svalues['Omnik 725'], "(%d+%.*%d*);(%d+%.*%d*)")
    diehlEnergy, diehlPower = string.match(otherdevices_svalues['Diehl (Youless)'], "(%d+%.*%d*);(%d+%.*%d*)")
-   power = (diehlPower + omnik715Power + omnik725Power)
-   energy = (diehlEnergy + omnik715Energy + omnik725Energy)
+   goodwePower, goodweEnergy = string.match(otherdevices_svalues['Goodwe'], "(%d+%.*%d*);(%d+%.*%d*)")
+   power = (goodwePower + diehlPower + omnik715Power + omnik725Power)
+   energy = (goodweEnergy + diehlEnergy + omnik715Energy + omnik725Energy)
+   omnikPower = (omnik715Power + omnik725Power)
+   omnikEnergy = (omnik715Energy + omnik725Energy)
    commandArray[1] = {['UpdateDevice'] = 31 .. "|0|" .. power .. ";" .. energy} -- 31 is the domoticz IDX
+   commandArray[2] = {['UpdateDevice'] = 35 .. "|0|" .. omnikPower .. ";" .. omnikEnergy} -- 35 is the domoticz IDX
 end
 
 return commandArray
