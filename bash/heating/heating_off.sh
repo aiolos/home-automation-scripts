c81e2997efb40250a8bad1faadbbeeba381ccfd9
Henri de Jong <henridejong@gmail.com>
2017-11-30 09:40:30 +0000

    Added heating scripts

---
+++ bash/heating/heating_off.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Setpoints to 12 degrees
+# IDX Thermostaat: 3459
+# IDX Study: 3511
+
+curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3459&nvalue=0&svalue=12
---
+++ bash/heating/kitchen_on.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Setpoints to 12 degrees
+# IDX Thermostaat: 3459
+# IDX Study: 3511
+
+curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3459&nvalue=0&svalue=20
+
+# Turn on valve, IDX?
+# curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3511&nvalue=0&svalue=21
+
+# Close other valves:
+curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3511&nvalue=0&svalue=13 # Study
+# curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3511&nvalue=0&svalue=13 # Living
