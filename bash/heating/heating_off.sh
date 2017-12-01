2380ddb0eb5e10ad3cf5e1ff7b36400ddf72e875
Henri de Jong <henridejong@gmail.com>
2017-12-01 09:27:01 +0000

    Fixing the heating files (#4)
    and adding fibonacci

--- bash/heating/heating_off.sh
+++ bash/heating/heating_off.sh
@@ -4,4 +4,4 @@
 # IDX Thermostaat: 3459
 # IDX Study: 3511
 
-curl -o /dev/null http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3459&nvalue=0&svalue=12
+curl -o /dev/null "http://192.168.38.110:8080/json.htm?type=command&param=udevice&idx=3459&nvalue=0&svalue=12"
