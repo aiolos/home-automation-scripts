3d731a3bf188238d274bab857ca326ea31ff92be
Henri de Jong <henridejong@gmail.com>
2018-01-19 17:15:13 +0000

    Add stop casting script

---
+++ radio/stopCasting.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+/home/pi/stream2chromecast/stream2chromecast.py -devicename 192.168.38.217 -stop &
+
+# Pioneer Off
+#curl -o /dev/null http://192.168.38.177/?code=A55AD827\&p=2\&r=5\&f=40\&d=20 &
