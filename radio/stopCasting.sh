46947147e729e1810d99ad3e63e666ba7af7b1a3
Henri de Jong <henridejong@gmail.com>
2018-01-19 18:56:47 +0000

    Added radio 2

--- radio/stopCasting.sh
+++ radio/stopCasting.sh
@@ -3,4 +3,4 @@
 /home/pi/stream2chromecast/stream2chromecast.py -devicename 192.168.38.217 -stop &
 
 # Pioneer Off
-#curl -o /dev/null http://192.168.38.177/?code=A55AD827\&p=2\&r=5\&f=40\&d=20 &
+curl -o /dev/null http://192.168.38.177/?code=A55AD827\&p=2\&r=5\&f=40\&d=20 &
