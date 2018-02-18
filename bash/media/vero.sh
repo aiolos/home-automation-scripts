e4bde1226b88084e40176e145c187fd3b7a71cc8
Henri de Jong <henridejong@gmail.com>
2018-02-18 19:45:45 +0000

    Sleep in IR scripts

--- bash/media/vero.sh
+++ bash/media/vero.sh
@@ -6,6 +6,9 @@ curl -o /dev/null http://192.168.38.220/control?cmd=IRSEND,PIONEER,0xA55A58A7,32
 # TV On
 curl -o /dev/null http://192.168.38.220/control?cmd=IRSEND,SAMSUNG,0xE0E09966,32 &
 
+# Wait till receiver and TV are turned on:
+sleep 2s
+
 # Receiver AppleTV input
 curl -o /dev/null http://192.168.38.220/control?cmd=IRSEND,PIONEER,0xA55AA15E,32,2 &
 
--- radio/castRadio1.sh
+++ radio/castRadio1.sh
@@ -4,5 +4,9 @@
 
 # receiver on
 curl -o /dev/null http://192.168.38.220/control?cmd=IRSEND,PIONEER,0xA55A58A7,32,2 &
+
+# Wait till receiver is turned on:
+sleep 3s
+
 # receiver A AUX
 curl -o /dev/null http://192.168.38.220/control?cmd=IRSEND,PIONEER,0xA55AF20D,32,2 &
