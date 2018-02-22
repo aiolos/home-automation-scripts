4b6b39cb7c2b624e7f58d1c0e4d610028a3d327e
Henri de Jong <henridejong@gmail.com>
2018-02-22 19:10:35 +0000

    Change scripts to work after reboot of Mopidy

--- radio/bedroom/radio1.sh
+++ radio/bedroom/radio1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Start playing radio 1
+curl -X POST -H Content-Type:application/json -d '{"method": "core.tracklist.clear","jsonrpc": "2.0","params": {},"id": 1}' http://192.168.38.225:6680/mopidy/rpc
+curl -X POST -H Content-Type:application/json -d '{"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add" , "params": {"uri": "http://icecast.omroep.nl/radio1-bb-mp3"}}' http://192.168.38.225:6680/mopidy/rpc
 curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.play",  "jsonrpc": "2.0", "params": { "tl_track": null, "tlid": 1 }, "id": 1 }' http://192.168.38.225:6680/mopidy/rpc
