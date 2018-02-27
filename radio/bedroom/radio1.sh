3bd56444c8b6f6723fe8a81ec26c788158fa7a3e
Henri de Jong <henridejong@gmail.com>
2018-02-27 15:47:49 +0000

    Update radio scripts

--- radio/bedroom/radio1.sh
+++ radio/bedroom/radio1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+URL=$(curl 'http://mhoom.enri.nl')
+PAYLOAD='{"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add" , "params": {"uri": "'${URL}'"}}'
+MOPIDY='http://192.168.38.225:6680/mopidy/rpc'
+
 # Start playing radio 1
-curl -X POST -H Content-Type:application/json -d '{"method": "core.tracklist.clear","jsonrpc": "2.0","params": {},"id": 1}' http://192.168.38.225:6680/mopidy/rpc
-curl -X POST -H Content-Type:application/json -d '{"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add" , "params": {"uri": "http://icecast.omroep.nl/radio1-bb-mp3"}}' http://192.168.38.225:6680/mopidy/rpc
-curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.play",  "jsonrpc": "2.0", "params": { "tl_track": null, "tlid": 1 }, "id": 1 }' http://192.168.38.225:6680/mopidy/rpc
+curl -X POST -H Content-Type:application/json -d '{"method": "core.tracklist.clear","jsonrpc": "2.0","params": {},"id": 1}' ${MOPIDY}
+curl -X POST -H Content-Type:application/json -d ${PAYLOAD} ${MOPIDY}
+curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.play",  "jsonrpc": "2.0", "params": { "tl_track": null, "tlid": 1 }, "id": 1 }' ${MOPIDY}
--- radio/bedroom/stop.sh
+++ radio/bedroom/stop.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.stop","jsonrpc": "2.0","params": {},"id": 1}' http://192.168.38.225:6680/mopidy/rpc
+MOPIDY='http://192.168.38.225:6680/mopidy/rpc'
+
+curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.stop","jsonrpc": "2.0","params": {},"id": 1}' ${MOPIDY}
