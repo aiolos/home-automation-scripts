fae33fe1bc1c64729d43911f3a94d82d3514dde8
Henri de Jong <henridejong@gmail.com>
2018-02-28 19:25:12 +0000

    Update payload quotes

--- radio/bedroom/radio1.sh
+++ radio/bedroom/radio1.sh
@@ -6,6 +6,6 @@ MOPIDY='http://192.168.38.225:6680/mopidy/rpc'
 
 # Start playing radio 1
 curl -X POST -H Content-Type:application/json -d '{"method": "core.tracklist.clear","jsonrpc": "2.0","params": {},"id": 1}' ${MOPIDY}
-curl -X POST -H Content-Type:application/json -d '${PAYLOAD}' ${MOPIDY}
+curl -X POST -H Content-Type:application/json -d "${PAYLOAD}" ${MOPIDY}
 curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.play",  "jsonrpc": "2.0", "params": { "tl_track": null, "tlid": 1 }, "id": 1 }' ${MOPIDY}
 curl -X POST -H Content-Type:application/json -d '{"method": "core.mixer.set_volume",  "jsonrpc": "2.0", "params": { "volume": 20 }, "id": 1 }' ${MOPIDY}
