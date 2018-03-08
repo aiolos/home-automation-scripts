74a5b7de6b53125f09da0f24718c37f226a5af50
Henri de Jong <henridejong@gmail.com>
2018-03-08 19:45:59 +0000

    Update volume / Add stream script

---
+++ orangepi/streamToFile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ffmpeg -t 3600 -i http://icecast.omroep.nl/radio1-bb-mp3 /media/usb/output.mp3
--- radio/bedroom/radio1.sh
+++ radio/bedroom/radio1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(curl 'http://mhoom.enri.nl')
+URL=$(curl 'http://mhoom.enri.nl/mhoom.m3u')
 PAYLOAD='{"jsonrpc": "2.0", "id": 1, "method": "core.tracklist.add" , "params": {"uri": "'${URL}'"}}'
 MOPIDY='http://192.168.38.225:6680/mopidy/rpc'
 
@@ -8,4 +8,4 @@ MOPIDY='http://192.168.38.225:6680/mopidy/rpc'
 curl -X POST -H Content-Type:application/json -d '{"method": "core.tracklist.clear","jsonrpc": "2.0","params": {},"id": 1}' ${MOPIDY}
 curl -X POST -H Content-Type:application/json -d "${PAYLOAD}" ${MOPIDY}
 curl -X POST -H Content-Type:application/json -d '{"method": "core.playback.play",  "jsonrpc": "2.0", "params": { "tl_track": null, "tlid": 1 }, "id": 1 }' ${MOPIDY}
-curl -X POST -H Content-Type:application/json -d '{"method": "core.mixer.set_volume",  "jsonrpc": "2.0", "params": { "volume": 20 }, "id": 1 }' ${MOPIDY}
+curl -X POST -H Content-Type:application/json -d '{"method": "core.mixer.set_volume",  "jsonrpc": "2.0", "params": { "volume": 100 }, "id": 1 }' ${MOPIDY}
