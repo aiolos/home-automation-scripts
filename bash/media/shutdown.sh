aa6d73f9e869a1ed1bc419d996ecac52d7fd42e5
Henri de Jong <henridejong@gmail.com>
2018-04-11 23:27:31 +0100

    Added service script for shutdown

--- bash/media/shutdown.sh
+++ bash/media/shutdown.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
 # Save this file in /storage/.config/shutdown.sh on OpenElec
-# Save this file in /lib/systemd/system-shutdown/shutdown.sh on OSMC
+
+# On OSMC:
+# Comment everything except the curl line in this file
+# Point to the right location of the script in shut.service
+# Save shut.service in /lib/systemd/system/shut.service
+# Reload: sudo systemctl daemon-reload
+# Check enabled with: sudo systemctl is-enabled shut
+# Check status: sudo systemctl status shut
+# More info here: https://osmc.tv/wiki/general/running-scripts-on-startup-and-shutdown/
 
 case "$1" in
   halt)
-    # your commands here
+    # halt commands here
     /usr/bin/curl "http://192.168.38.110:8080/json.htm?type=command&param=switchscene&idx=4&switchcmd=Off"
     ;;
   poweroff)
-    # your commands here
+    # poweroff commands here
     /usr/bin/curl "http://192.168.38.110:8080/json.htm?type=command&param=switchscene&idx=4&switchcmd=Off"
     ;;
   reboot)
-    # your commands here
+    # reboot commands here
     ;;
   *)
-    # your commands here
+    # other commands here
     ;;
 esac
